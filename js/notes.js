05aa8acd50d9caea8763291e168723a3ded20cc4
Adam Ingram <everett.ingram22@gmail.com>
2019-12-06 14:12:04 -0500

    Retrieving Pokemon Data from the PokeAPI

--- js/notes.js
+++ js/notes.js
@@ -1,5 +1,28 @@
 
-
+//***Retrieving Pokemon Data from the PokeAPI***
+//   var pokeapi = "https://pokeapi.co/api/v2/generation/1";
+//
+//   $.getJSON(pokeapi).done(function(data) {
+//     console.log(data);  // Explore available data in the Developer Tools Console
+//     $.each(data.pokemon_species, function(index, pokemon) {
+//       var name = capitalize(pokemon.name);
+//       var paragraph = $("<p>").html("Pokémon species no. " + (index+1) + " is " + name);
+//       paragraph.appendTo("#pokedex");
+//     });
+//   }).fail(function() {
+//     // Handle error case
+//     console.log("Call to PokéAPI failed.");
+//   }).always(function() {
+//     // Will always be executed (in either case).
+//     console.log("Pokémon is awesome.")
+//   });
+//
+// });
+//
+// // Capitalizes a given string.
+// function capitalize(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
 
 //***Retrieving Flicker Images Through the Flickr API***
 //JSON data format: this is the normal javascript object notation {key: value, key2: value}
--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 $(function () {
 
   //***Retrieving Pokemon Data from the PokeAPI***
-  var pokeapi = "https://pokeapi.co/api/v2/generation/1";
+  let pokeapiUrl = "https://pokeapi.co/api/v2/generation/1";
+  let pokeapiByNameUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-  $.getJSON(pokeapi).done(function(data) {
+  $.getJSON(pokeapiUrl).done(function(data) {
     console.log(data);  // Explore available data in the Developer Tools Console
     $.each(data.pokemon_species, function(index, pokemon) {
-      var name = capitalize(pokemon.name);
-      var paragraph = $("<p>").html("Pokémon species no. " + (index+1) + " is " + name);
+      let name = capitalize(pokemon.name);
+      let link = $("<a>").attr("id", pokemon.name).attr("href", "#").append($("<strong>").text(name));
+      let paragraph = $("<p>").html("Pokémon species no. " + (index+1) + " is ").append(link);
+
+      link.click(function (event) {
+        $.getJSON(pokeapiByNameUrl + pokemon.name).done(function (details) {
+          console.log(details);
+          let pokemonDiv = $("#pokemon-details");
+          pokemonDiv.empty();
+          pokemonDiv.append("<h2>" + name + "</h2>");
+          pokemonDiv.append("<img src='" + details.sprites.front_default + "'>");
+          pokemonDiv.append("<img src='" + details.sprites.back_default + "'>");
+          pokemonDiv.append("<img src='" + details.sprites.front_shiny + "'>");
+          pokemonDiv.append("<img src='" + details.sprites.back_shiny + "'>");
+        });
+        event.preventDefault();
+      });
+
       paragraph.appendTo("#pokedex");
     });
   }).fail(function() {
