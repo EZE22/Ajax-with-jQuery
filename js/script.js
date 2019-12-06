12a12c9ee183a4b7093c40f9e8ebd4b772d53704
Adam Ingram <everett.ingram22@gmail.com>
2019-12-06 12:09:21 -0500

    Fetching a Server File with JQuery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
+  //$.get(), $.post(), $.ajax(), $.getJSON()
+
+  //***Fetching a Server File with JQuery***
+  //jquery function called $.load()
+  $("#code").load("idontexist.php", function (response, status) {
+    if(status === "error"){
+      alert("could not find idontexist.php");
+    }
+    console.log(response);
+  });
 
 });
